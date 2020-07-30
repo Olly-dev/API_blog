ef6bd1655f4fd9f4cdaf5088ae9f6ebfe10f706e
Olly-dev <lemraouiadil@yahoo.fr>
2020-07-30 20:34:34 +0200

    refacto Authorization and Events

---
+++ API_blog/src/Authorizations/ResourceAccessCheckerInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Authorizations;
+
+interface ResourceAccessCheckerInterface
+{
+    const MESSAGE_ERROR = "it's not your ressource";
+    public function canAccess(?int $id): void;
+}
---
+++ API_blog/src/Events/ResourceUpdatorSubscriber.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Events;
+
+use ApiPlatform\Core\EventListener\EventPriorities;
+use App\Entity\User;
+use App\Authorizations\UserAuthorizationChecker;
+use App\Entity\Article;
+use App\Services\ResourceUpdatorInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class ResourceUpdatorSubscriber implements EventSubscriberInterface
+{
+    
+    private ResourceUpdatorInterface $resourceUpdator;
+
+    public function __construct(ResourceUpdatorInterface $resourceUpdator)
+    {
+        $this->resourceUpdator = $resourceUpdator;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::VIEW => ['check', EventPriorities::PRE_VALIDATE]
+        ];
+    }
+
+    public function check(ViewEvent $event): void
+    {
+        $object = $event->getControllerResult();
+
+        if ($object instanceof User || $object instanceof Article) {
+            $user = $object instanceof User ? $object : $object->getAuthor();
+            $canProcess = $this->resourceUpdator->process(
+                        $event->getRequest()->getMethod(), 
+                        $user
+                    );
+            
+            if($canProcess) {
+                $user->setUpdatedAt(new \DateTimeImmutable());
+            }
+            
+        }
+    }
+}
